9c038ac00fd1e42cdab1ca066a9f7921f8e8426f
David Mihal <david@dmihal.com>
2020-07-20 22:15:58 +0100

    Use Fuel API for search queries

--- pages/api/search/[query].ts
+++ pages/api/search/[query].ts
@@ -1,28 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { getBlock } from 'data/blocks';
-import { getRoot } from 'data/roots';
-import { getTransaction } from 'data/transactions';
-// import { getAddresses } from 'data/addresses';
 import { isUInt } from 'utils/str';
+import api from 'data/api';
 
 const addressRegex = /0x[0-9a-f]{40}/i;
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const query = req.query.query as string;
     let page = null;
     let path = null;
 
     if (isUInt(query)) {
-      if (getBlock(parseInt(query))) {
+      if (await api.getBlockByHeight(parseInt(query))) {
         page = '/block/[blockNum]';
         path = `/block/${query}`;
       }
     } else {
-      if (getTransaction(query)) {
+      if (await api.getTransactionByHash(query)) {
         page = '/transaction/[txHash]';
         path = `/transaction/${query}`;
-      } else if (getRoot(query)) {
+      } else if (await api.getRootByHash(query)) {
         page = '/root/[rootHash]';
         path = `/root/${query}`;
       } else if (addressRegex.test(query)) {
