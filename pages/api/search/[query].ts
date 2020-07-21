112d11ad48bfded092ed9099a15eae3554281c59
David Mihal <david@dmihal.com>
2020-07-21 20:11:55 +0100

    Improve search API

--- pages/api/search/[query].ts
+++ pages/api/search/[query].ts
@@ -4,6 +4,8 @@ import api from 'data/api';
 
 const addressRegex = /0x[0-9a-f]{40}/i;
 
+const noOp = () => void null;
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const query = req.query.query as string;
@@ -11,15 +13,18 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     let path = null;
 
     if (isUInt(query)) {
-      if (await api.getBlockByHeight(parseInt(query))) {
+      if (await api.getBlockByHeight(parseInt(query)).catch(noOp)) {
         page = '/block/[blockNum]';
         path = `/block/${query}`;
       }
-    } else {
-      if (await api.getTransactionByHash(query)) {
+    } else if (query.indexOf('0x') === 0) {
+      const isTx = api.getTransactionByHash(query).catch(noOp);
+      const isRoot = api.getRootByHash(query).catch(noOp);
+
+      if (await isTx) {
         page = '/transaction/[txHash]';
         path = `/transaction/${query}`;
-      } else if (await api.getRootByHash(query)) {
+      } else if (await isRoot) {
         page = '/root/[rootHash]';
         path = `/root/${query}`;
       } else if (addressRegex.test(query)) {
@@ -30,7 +35,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     res.status(200).json({ path, page });
   } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message })
+    res.status(500).json({ statusCode: 500, message: err.message || err })
   }
 }
 
