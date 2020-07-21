b04ee2e0f46fb62733a0a3bb8dc0d16d0f6b6137
David Mihal <david@dmihal.com>
2020-07-21 13:42:55 +0100

    Copy button in header

--- components/SubHeader.tsx
+++ components/SubHeader.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import QRCode from 'qrcode.react';
 import CopyToClipboard from 'react-copy-to-clipboard';
+import copyIcon from 'assets/copy.svg';
 import qricon from 'assets/qr-icon.svg';
 import Modal from './Modal';
 
@@ -13,8 +14,12 @@ interface SubHeaderProps {
 
 const SubHeader: React.FC<SubHeaderProps> = ({ type, inline, children, qr, copy }) => {
   const [showQR, setShowQR] = useState(false);
+  const [copied, setCopied] = useState(false);
 
-  const onCopy = () => null; //TODO
+  const onCopy = () => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2500);
+  };
   return (
     <h2 className={inline ? 'inline' : ''}>
       <div className="primary">{type}{!inline && ':'}</div>
@@ -23,7 +28,10 @@ const SubHeader: React.FC<SubHeaderProps> = ({ type, inline, children, qr, copy 
 
         {copy && (
           <CopyToClipboard text={children as string} onCopy={onCopy}>
-            <button className="button" />
+            <button
+              className={`button copy-btn ${copied ? 'copied' : ''}`}
+              style={{ backgroundImage: `url('${copyIcon}')` }}
+            />
           </CopyToClipboard>
         )}
 
@@ -83,10 +91,21 @@ const SubHeader: React.FC<SubHeaderProps> = ({ type, inline, children, qr, copy 
           background-size: 60%;
           background-repeat: no-repeat;
           background-position: center;
+          flex: 0 0 24px;
         }
 
         .button:hover {
           background-color: rgba(130, 255, 239, 0.17);
+        }
+
+        .copy-btn.copied:after {
+          content: 'Copied';
+          position: absolute;
+          transform: translate(-50%, -36px);
+          background: #04c399;
+          padding: 4px;
+          border-radius: 4px;
+          opacity: 0.17;
         }
 
         .qrContainer {
